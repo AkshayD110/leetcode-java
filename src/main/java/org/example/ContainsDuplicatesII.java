88b7791a35bd6acb71d44688761edf45468738e4
AkshayD110 <akshayd110@gmail.com>
2024-06-16 20:27:36 +0530

    Sliding window: some notes

--- src/main/java/org/example/ContainsDuplicatesII.java
+++ src/main/java/org/example/ContainsDuplicatesII.java
@@ -22,12 +22,14 @@ public class ContainsDuplicatesII {
     }
 
     public boolean containsNearbyDuplicateBetter(int[] nums, int k) {
+        //in this, a set is used to keep track if contition (i-j<=k)
         Set<Integer> set = new LinkedHashSet<>();
         for (int i=0; i< nums.length; i++){
             if (set.contains(nums[i])){
                 return true;
             }else {
                 set.add(nums[i]);
+                //the below part is not very intutive to think of.
                 if (set.size() > k){
                     set.remove(nums[i-k]);
                 }
