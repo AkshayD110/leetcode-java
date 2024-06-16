2b0842fb7b5a13b9d94141c6afc120c81f1eb4bd
AkshayD110 <akshayd110@gmail.com>
2024-06-16 11:19:24 +0530

    Sliding window: using set O(n)

--- src/main/java/org/example/ContainsDuplicatesII.java
+++ src/main/java/org/example/ContainsDuplicatesII.java
@@ -1,9 +1,15 @@
 package org.example;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class ContainsDuplicatesII {
     //Brute force
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         for (int i=0; i< nums.length; i++){
+            //important to start the loop from after i
             for (int j=i+1; j<nums.length; j++){
                 if (nums[i] == nums[j]){
                     if((j-i) <=k){
@@ -16,7 +22,17 @@ public class ContainsDuplicatesII {
     }
 
     public boolean containsNearbyDuplicateBetter(int[] nums, int k) {
-
+        Set<Integer> set = new LinkedHashSet<>();
+        for (int i=0; i< nums.length; i++){
+            if (set.contains(nums[i])){
+                return true;
+            }else {
+                set.add(nums[i]);
+                if (set.size() > k){
+                    set.remove(nums[i-k]);
+                }
+            }
+        }
         return false;
     }
 }
