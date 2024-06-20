f1c6685aceedb93d00a3b2c744c532b5cbb321df
AkshayD110 <akshayd110@gmail.com>
2024-06-20 10:02:11 +0530

    Burte force optimized

--- src/main/java/org/example/LenghtOfLongestValidSubstring.java
+++ src/main/java/org/example/LenghtOfLongestValidSubstring.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import javax.swing.text.AttributeSet;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
@@ -8,25 +9,28 @@ import java.util.Set;
 public class LenghtOfLongestValidSubstring {
     //brute force
     public int longestValidSubstring(String word, List<String> forbidden) {
+        int maxSubStringLen=0;
         Set<String> set = new HashSet();
-        for (int i=0; i<word.length(); i++){
-            set.add(Character.toString(word.charAt(i)));
-        }
 
         for (int i=0; i<word.length(); i++){
             String curretSub=Character.toString(word.charAt(i));
+            if(!isForbidden(curretSub, forbidden)){
+                set.add(Character.toString(word.charAt(i)));
+                maxSubStringLen=Math.max(curretSub.length(), maxSubStringLen);
+            }
             for (int j=i+1; j<word.length(); j++) {
                 curretSub = (curretSub + (word.charAt(j)));
-                if(!isForbidden(curretSub, forbidden)){
+                if(isForbidden(curretSub, forbidden)){
+                    continue;
+                }
+                else{
                     set.add(curretSub);
+                    maxSubStringLen=Math.max(curretSub.length(), maxSubStringLen);
                     }
                 }
             }
 
-        for (Object element:set){
-            System.out.println(element);
-        }
-        return set.size();
+        return maxSubStringLen;
     }
 
     public boolean isForbidden(String currentSubString, List<String> forbidden){
@@ -41,8 +45,4 @@ public class LenghtOfLongestValidSubstring {
         return result;
     }
 
-    public int longestSubArray(Set<String> allValidSubString){
-        //write logic for bruteforce here
-        return 0;
-    }
 }
